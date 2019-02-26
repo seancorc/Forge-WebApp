a54aad04d50f39ef422fcb2d9762e57dfe339e41
Sean Corcoran <seancorcoran45@icloud.com>
2019-02-25 23:09:44 -0500

    inital commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>Forge</p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
